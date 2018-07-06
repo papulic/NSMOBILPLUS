151198c88610f30c18709976e065cd6e848c5222
papulic <papulic@gmail.com>
2018-07-06 14:56:14 +0200

    Add files via upload

--- eprodaja/forms.py
+++ eprodaja/forms.py
@@ -4,16 +4,16 @@ from django.contrib.auth.models import User
 
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email Adresa ili korisnicko ime', 'title': ' '}), label='')
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Lozinka', 'title': ' '}), label='')
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email Adresa ili korisnicko ime', 'title': ' ', 'id': 'username_login'}), label='')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Lozinka', 'title': ' ', 'id': 'password_login', 'autocomplete': 'off'}), label='')
 
 class UserRegisterForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Korisnicko ime', 'title': ' '}), label='')
     ime = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ime', 'title': ' '}), label='')
     prezime = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Prezime', 'title': ' '}), label='')
     email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'Email Adresa', 'title': ' '}), label='')
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Lozinka', 'title': ' '}), label='')
-    password_2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Ponovite lozinku', 'title': ' '}), label='')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Lozinka', 'title': ' ', 'autocomplete': 'off'}), label='')
+    password_2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Ponovite lozinku', 'title': ' ', 'autocomplete': 'off'}), label='')
 
 class DetaljiForm(forms.Form):
     adresa = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ulica i broj', 'title': ' '}), label='', required=False)
