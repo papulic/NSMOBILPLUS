f372e28378a77775f1f8efd7cdbb895b5effd6ff
papulic <papulic@gmail.com>
2018-07-13 09:29:38 +0200

    Add files via upload

--- nsmobilplus/settings.py
+++ nsmobilplus/settings.py
@@ -134,6 +134,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
