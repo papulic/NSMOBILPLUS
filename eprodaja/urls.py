14e955f22a61a226a421c1689702378c426f7d4d
papulic <papulic@gmail.com>
2018-07-13 09:29:20 +0200

    Add files via upload

--- eprodaja/urls.py
+++ eprodaja/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 
 app_name = 'eprodaja'
@@ -20,4 +21,6 @@ urlpatterns = [
     url(r'^pretraga/$', views.pretraga, name='pretraga'),
     url(r'^create_modal/$', views.create_modal, name='create_modal'),
     # url(r'^artikal/(?P<artikal_id>[0-9]+)/$', views.artikal_detalji, name='artikal_detalji'), # treba ako nema jquery
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+]
